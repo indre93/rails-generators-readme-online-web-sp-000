cff7e5f25059c4afb87270517c929df5ad71ed7a
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-21 03:16:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190821031531) do
+ActiveRecord::Schema.define(version: 20190821031626) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
